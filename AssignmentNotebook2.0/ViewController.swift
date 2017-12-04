52d1c6a99f3c30db98b36b1bfcd1231bb289e944
HSDeCecco <beeHSD2720@gmail.com>
2017-12-04 10:14:23 -0500

    YU[

--- AssignmentNotebook2.0/ViewController.swift
+++ AssignmentNotebook2.0/ViewController.swift
@@ -17,10 +17,7 @@ class ViewController: UIViewController {
        //apples
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
 
 
 }
